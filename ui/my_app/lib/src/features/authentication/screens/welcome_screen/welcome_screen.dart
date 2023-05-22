b9b7bf768df1002debbc05bd66b295d9ce582212
Pooja Shah <pooja.shah@sap.com>
2023-05-22 22:52:46 +0530

    formatted

--- ui/my_app/lib/src/features/authentication/screens/welcome_screen/welcome_screen.dart
+++ ui/my_app/lib/src/features/authentication/screens/welcome_screen/welcome_screen.dart
@@ -17,37 +17,58 @@ class WelcomeScreen extends StatelessWidget {
     controller.startAnimation();
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Stack(children : [
-          TFadeInAnimation(durationInMs: 1200,
-          animate: TAmimatePosition(bottomAfter:0, bottomBefore: -100,leftAfter: 0,leftBefore: 0,rightAfter: 0,rightBefore: 0,topAfter: 0,topBefore: 0),
-          child:Container(
-          padding: const EdgeInsets.all(tDefaultSize),
-      child:
-          Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-        Image(image: AssetImage(welcomeScreenImage2), height: height * 0.6),
-        Column(children: [Text("Welcome Title"), Text("Welcome Subtitle")]),
-        Row(
-          children: [
-            Expanded(
-                child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: tButtonHeight)),
-                    onPressed: () {Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => const LoginScreen()),
-  );},
-                    child: Text(tLogin.toUpperCase()))),
-            SizedBox(width: 10.0),
-            Expanded(
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: tButtonHeight)),
-                    onPressed: () {},
-                    child: Text(tSignUP.toUpperCase())))
-          ],
-        )
-        ]),
-    ))]));
+        body: Stack(children: [
+      TFadeInAnimation(
+          durationInMs: 1200,
+          animate: TAmimatePosition(
+              bottomAfter: 0,
+              bottomBefore: -100,
+              leftAfter: 0,
+              leftBefore: 0,
+              rightAfter: 0,
+              rightBefore: 0,
+              topAfter: 0,
+              topBefore: 0),
+          child: Container(
+            padding: const EdgeInsets.all(tDefaultSize),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image(
+                      image: AssetImage(welcomeScreenImage2),
+                      height: height * 0.6),
+                  Column(children: [
+                    Text("Welcome Title"),
+                    Text("Welcome Subtitle")
+                  ]),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: OutlinedButton(
+                              style: OutlinedButton.styleFrom(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: tButtonHeight)),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const LoginScreen()),
+                                );
+                              },
+                              child: Text(tLogin.toUpperCase()))),
+                      SizedBox(width: 10.0),
+                      Expanded(
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: tButtonHeight)),
+                              onPressed: () {},
+                              child: Text(tSignUP.toUpperCase())))
+                    ],
+                  )
+                ]),
+          ))
+    ]));
   }
 }
