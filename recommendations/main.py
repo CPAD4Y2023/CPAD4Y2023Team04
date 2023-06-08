daf9767bfecdf27ea632c7150534400dd8745c04
I513949 <pooja.shah@sap.com>
2023-06-08 18:31:29 +0530

    deployment and postman collection

--- recommendations/main.py
+++ recommendations/main.py
@@ -14,26 +14,26 @@ def hello_world():
 	return 'Hello World'
 
 
-@app.route('/degreeCourses')
+@app.route('/v1/degreeCourses')
 def suggest_degree_courses():
       hobbies = request.args.get("hobbies")
       skills = request.args.get("skills")
       personality = request.args.get("personality")
-      return getDegreeCourses(hobbies, skills, personality).replace("\n","").split("- ")
+      return getDegreeCourses(hobbies, skills, personality).replace("\n","").split("- ")[1:]
 
-@app.route('/colleges')
+@app.route('/v1/colleges')
 def suggested_colleges():
       subject = request.args.get("subject")
-      return getCollegeNames(subject).replace("\n","").split("- ")
+      return getCollegeNames(subject).replace("\n","").split("- ")[1:]
 
-@app.route('/course')
+@app.route('/v1/course')
 def suggested_courses():
       skills = request.args.get("skills")
       return get_courses(skills)
 
-@app.route('/latestNews')
+@app.route('/v1/latestNews')
 def fetch_latest_news():
-      skills = request.args.get("skills")
+      skills = request.args.get("topics")
       return get_latest_news(skills)
 
 
@@ -44,4 +44,4 @@ if __name__ == '__main__':
 
 	# run() method of Flask class runs the application
 	# on the local development server.
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
